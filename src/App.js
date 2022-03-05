ea0e61dbe8541ac6a986f3e33d9e0833d9a60e11
Oscar Aldana Assad <oaa97181@hotmail.com>
2022-03-05 14:01:13 -0600

    delete unused code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import { useFeaturedBanners } from './utils/hooks/useFeaturedBanners';
+import {useFeaturedBanners} from './utils/hooks/useFeaturedBanners';
+import Navbar from "./components/Navbar";
 
 function App() {
-  const { data, isLoading } = useFeaturedBanners();
-  console.log(data, isLoading);
+    const {data, isLoading} = useFeaturedBanners();
+    console.log(data, isLoading);
 
-  return (
-    <div >
-      <header>
-o
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Navbar/>
+        </>
+    );
 }
 
 export default App;
