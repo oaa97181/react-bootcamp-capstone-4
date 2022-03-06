e3acf3b1364dfc4553ca124bd194cfb604c0af14
Oscar Aldana Assad <oaa97181@hotmail.com>
2022-03-06 14:12:44 -0600

    create ProductGrid component :DD

--- src/components/Homepage/ProductGrid/index.js
+++ src/components/Homepage/ProductGrid/index.js
@@ -1,9 +1,20 @@
 import styles from "./styles.module.css";
+import ProductCard from "../../ReusableComponents/ProductCard";
 
 function ProductGrid({data}) {
+
     return (
         <>
-            featured products
+            <div className={styles.productGridContainer}>
+                <div className={styles.subtitle}>
+                    <h2>featured products</h2>
+                </div>
+                {data.results.map((product) => {
+                    return (
+                        <ProductCard product={product}/>
+                    )
+                })}
+            </div>
         </>
     );
 }
