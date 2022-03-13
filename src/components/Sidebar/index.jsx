b2414319ac19ce5a2d81ab965b981d77fd5f8c34
Oscar Aldana Assad <oaa97181@hotmail.com>
2022-03-12 23:32:34 -0600

    add prop types and remove unused code

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,27 +1,19 @@
 import styles from "./styles.module.css";
 import PropTypes from "prop-types";
-import {useEffect, useState} from "react";
 
-function Sidebar({productCategories}) {
-
-    const [categoryArray, setCategoryArray] = useState([]);
+function Sidebar({productCategories, categoryArray, setCategoryArray}) {
 
     function handleChange(e, value) {
         // eslint-disable-next-line max-len
         return categoryArray.indexOf(value) === -1 ? setCategoryArray([...categoryArray, value]) : setCategoryArray(categoryArray.filter(item => item !== value));
     }
 
-    useEffect(() => {
-        console.log(categoryArray)
-    }, [categoryArray]);
-
-
     return (
         <>
             {productCategories.results.map(category => {
                 return (
                     <div key={category.data.name} onChange={(e) => {
-                        handleChange(e, category.data.name)
+                        handleChange(e, category.data.name.toLowerCase())
                     }}>
                         <input type="checkbox" name={category.data.name}
                                value={category.data.name}/>
@@ -36,6 +28,8 @@ function Sidebar({productCategories}) {
 
 Sidebar.propTypes = {
     productCategories: PropTypes.object.isRequired,
+    categoryArray:PropTypes.array.isRequired,
+    setCategoryArray:PropTypes.func.isRequired,
 };
 
 export default Sidebar;
