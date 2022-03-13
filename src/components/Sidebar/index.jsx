2ddc336217af23b594ab36e5b6102d76b4828cc2
Oscar Aldana Assad <oaa97181@hotmail.com>
2022-03-12 22:52:42 -0600

    correctly create categoryArray using state @Sidebar :DDD

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,11 +1,41 @@
 import styles from "./styles.module.css";
+import PropTypes from "prop-types";
+import {useEffect, useState} from "react";
 
-function Sidebar() {
+function Sidebar({productCategories}) {
+
+    const [categoryArray, setCategoryArray] = useState([]);
+
+    function handleChange(e, value) {
+        // eslint-disable-next-line max-len
+        return categoryArray.indexOf(value) === -1 ? setCategoryArray([...categoryArray, value]) : setCategoryArray(categoryArray.filter(item => item !== value));
+    }
+
+    useEffect(() => {
+        console.log(categoryArray)
+    }, [categoryArray]);
+
+
     return (
         <>
-sidebar goees hereeee
+            {productCategories.results.map(category => {
+                return (
+                    <div key={category.data.name} onChange={(e) => {
+                        handleChange(e, category.data.name)
+                    }}>
+                        <input type="checkbox" name={category.data.name}
+                               value={category.data.name}/>
+                        <label> {category.data.name}</label>
+                    </div>
+                )
+            })}
         </>
     );
 }
+
+
+Sidebar.propTypes = {
+    productCategories: PropTypes.object.isRequired,
+};
 
 export default Sidebar;
