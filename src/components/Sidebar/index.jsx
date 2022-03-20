253591c445307c17622fd35b09fa7083d59a0a7a
Oscar Aldana Assad <oaa97181@hotmail.com>
2022-03-19 23:38:45 -0600

    Refactor Product Categories Sidebar to work w API

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,7 +1,10 @@
 import "./styles.css";
 import PropTypes from "prop-types";
+import {useWizelineData} from "../../utils/hooks/useWizelineData";
 
-function Sidebar({productCategories, categoryArray, setCategoryArray}) {
+function Sidebar({categoryArray, setCategoryArray}) {
+
+    const {data, isLoading} = useWizelineData('category', 30);
 
     function handleChange(e, value) {
         return categoryArray.indexOf(value) === -1 ?
@@ -22,32 +25,40 @@ function Sidebar({productCategories, categoryArray, setCategoryArray}) {
 
     return (
         <>
-            <button className="sidebar-button" onClick={openSidebar} id='sidebar-button'>☰</button>
+            {
+                !isLoading &&
+                <>
+                    <button className="sidebar-button" onClick={openSidebar}
+                            id='sidebar-button'>☰
+                    </button>
 
-            <div className="sidebar-container" id="sidebar-container">
+                    <div className="sidebar-container" id="sidebar-container">
 
-                <button className="close-button" onClick={closeSidebar}>Close &times;</button>
+                        <button className="close-button"
+                                onClick={closeSidebar}>Close &times;</button>
 
-                {productCategories.results.map(category => {
-                    return (
-                        <div key={category.data.name} className="sidebar-filter"
-                             onChange={(e) => {
-                                 handleChange(e, category.data.name.toLowerCase())
-                             }}>
-                            <input type="checkbox" name={category.data.name}
-                                   value={category.data.name}/>
-                            <label> {category.data.name}</label>
-                        </div>
-                    )
-                })}
-            </div>
+                        {data.results.map(category => {
+                            console.log(category.data)
+                            return (
+                                <div key={category.data.name} className="sidebar-filter"
+                                     onChange={(e) => {
+                                         handleChange(e, category.data.name.toLowerCase())
+                                     }}>
+                                    <input type="checkbox" name={category.data.name}
+                                           value={category.data.name}/>
+                                    <label> {category.data.name}</label>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </>
+            }
         </>
     );
 }
 
 
 Sidebar.propTypes = {
-    productCategories: PropTypes.object.isRequired,
     categoryArray: PropTypes.array.isRequired,
     setCategoryArray: PropTypes.func.isRequired,
 };
--- src/pages/ProductList/index.jsx
+++ src/pages/ProductList/index.jsx
@@ -1,7 +1,6 @@
 import ProductGrid from "../../components/ReusableComponents/ProductGrid";
 import Sidebar from "../../components/Sidebar";
 import products from '../../data/mocks/en-us/products.json'
-import productCategories from '../../data/mocks/en-us/product-categories.json'
 import {useState} from "react";
 import LoadingComponent from "../../components/ReusableComponents/LoadingComponent";
 
@@ -13,9 +12,7 @@ function ProductList() {
     return (
         <>
             <LoadingComponent data={products}/>
-
-            <Sidebar productCategories={productCategories} categoryArray={categoryArray}
-                     setCategoryArray={setCategoryArray}/>
+            <Sidebar categoryArray={categoryArray} setCategoryArray={setCategoryArray}/>
             <ProductGrid products={products} title='All Products' categoryArray={categoryArray}/>
         </>
     );
