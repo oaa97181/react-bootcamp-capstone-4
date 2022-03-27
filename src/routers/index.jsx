37cc9dd17ddab785c36f008f91ef439f20b83737
Oscar Aldana Assad <oaa97181@hotmail.com>
2022-03-26 19:56:41 -0600

    create AppRouter n.n

---
+++ src/routers/index.jsx
@@ -0,0 +1,30 @@
+import Navbar from "../components/ReusableComponents/Navbar";
+import Footer from "../components/ReusableComponents/Footer";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route, Redirect,
+} from "react-router-dom";
+import Homepage from "../pages/Homepage";
+import ProductList from "../pages/ProductList";
+import ProductDetail from "../pages/ProductDetail";
+
+
+function AppRouter() {
+
+    return (
+        <Router>
+            <Navbar/>
+            <Switch>
+                <Route path="/home" component={Homepage}/>
+                <Route path='/products' component={ProductList}/>
+                <Route path='/product/:productId' component={ProductDetail}/>
+                <Route path='/search' component={ProductList}/>
+                <Redirect to="/home"/>
+            </Switch>
+            <Footer/>
+        </Router>
+    );
+}
+
+export default AppRouter;
