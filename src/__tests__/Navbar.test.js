8b1434ae09d752d82623832cb692839bad80af59
Oscar Aldana Assad <oaa97181@hotmail.com>
2022-04-09 18:08:13 -0500

    fix Navbar.test.js :DDD

--- src/__tests__/Navbar.test.js
+++ src/__tests__/Navbar.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import Navbar from "../components/ReusableComponents/Navbar";
+import {BrowserRouter} from "react-router-dom";
 
 test('renders shop name/title in Navbar', async () => {
-  render(<Navbar/>);
-  const title = await screen.findByText('React Shop');
-  expect(title.textContent).toBe('React Shop');
+    render(
+        <BrowserRouter>
+            <Navbar/>
+        </BrowserRouter>
+    );
+    const title = await screen.findByText('React Shop');
+    expect(title.textContent).toBe('React Shop');
 });
