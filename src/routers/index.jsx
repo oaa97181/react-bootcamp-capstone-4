21759c6ecd346569dbecebdcaf4f1c4b2bf32bde
Oscar Aldana Assad <oaa97181@hotmail.com>
2022-04-10 15:44:40 -0500

    add initial state only in LOCLAHOST n.n

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -15,27 +15,51 @@ import {CartReducer} from "../contexts/CartReducer";
 import {useReducer} from "react";
 
 function AppRouter() {
-    let singleProduct = {
-        category: {
-            id: 'YWHyYRIAACgAykCq',
-            type: 'category',
-            tags: Array(0),
-            lang: 'en-us',
-            slug: 'decorate',
+    
+    let prod1 = {
+
+        singleProduct: {
+            category: {
+                id: 'YWHyYRIAACgAykCq',
+                type: 'category',
+                lang: 'en-us',
+                slug: 'decorate',
+            },
+            mainimage: {
+                url:
+                    "https://images.prismic.io/wizeline-academy/1c8883ca-" +
+                    "2797-4138-a7e5-a7d2ae583192_1.jpeg?auto=compress,format",
+            },
+            name: "Fair Isle Snowflake Lumbar Cushion Cover",
+            price: 40,
+            sku: "1107982309",
+            stock: 48,
         },
-        mainimage: {
-            url:
-                "https://images.prismic.io/wizeline-academy/1c8883ca-" +
-                "2797-4138-a7e5-a7d2ae583192_1.jpeg?auto=compress,format",
+        units: `1`,
+    }
+    let prod2 = {
+        singleProduct: {
+            category: {
+                id: 'testid',
+                type: 'category',
+                lang: 'en-us',
+                slug: 'decorate',
+            },
+            mainimage: {
+                url:
+                    "https://images.prismic.io/wizeline-academy/1c8883ca-" +
+                    "2797-4138-a7e5-a7d2ae583192_1.jpeg?auto=compress,format",
+            },
+            name: "testname",
+            price: 99,
+            sku: "testSKU",
+            stock: 99,
         },
-        name: "Fair Isle Snowflake Lumbar Cushion Cover",
-        price: 40,
-        sku: "1107982309",
-        stock: 48,
-    };
-    let units = `1`;
+        units: `1`,
+    }
+
     const [state, dispatch] = useReducer(CartReducer, {
-        products: [{singleProduct, units}],
+        products: window.location.hostname.includes('localhost')?[prod1, prod2]:[],
     });
 
     return (
