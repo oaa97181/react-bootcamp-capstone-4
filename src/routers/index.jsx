41c5d3e4e5b09ce9c20cd26c99f64e617a13588b
Oscar Aldana Assad <oaa97181@hotmail.com>
2022-04-10 15:03:32 -0500

    add initial state :D

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -15,8 +15,27 @@ import {CartReducer} from "../contexts/CartReducer";
 import {useReducer} from "react";
 
 function AppRouter() {
+    let singleProduct = {
+        category: {
+            id: 'YWHyYRIAACgAykCq',
+            type: 'category',
+            tags: Array(0),
+            lang: 'en-us',
+            slug: 'decorate',
+        },
+        mainimage: {
+            url:
+                "https://images.prismic.io/wizeline-academy/1c8883ca-" +
+                "2797-4138-a7e5-a7d2ae583192_1.jpeg?auto=compress,format",
+        },
+        name: "Fair Isle Snowflake Lumbar Cushion Cover",
+        price: 40,
+        sku: "1107982309",
+        stock: 48,
+    };
+    let units = `1`;
     const [state, dispatch] = useReducer(CartReducer, {
-        products: [],
+        products: [{singleProduct, units}],
     });
 
     return (
